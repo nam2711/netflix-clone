5b8f4213de497baeb84dd71c7ed9fad64265f34c
Nam <nam2711@hotmail.fr>
2020-11-17 17:00:46 +0100

    adding sign up sign in page using firebase

--- src/components/opt-form/styles/opt-form.js
+++ src/components/opt-form/styles/opt-form.js
@@ -6,7 +6,6 @@ export const Container = styled.div`
   height: 100%;
   margin-top: 20px;
   flex-wrap: wrap;
-
   @media (max-width: 1000px) {
     flex-direction: column;
     align-items: center;
@@ -22,9 +21,15 @@ export const Input = styled.input`
   box-sizing: border-box;
 `;
 
+export const Break = styled.div`
+  flex-basis: 100%;
+  height: 0;
+`;
+
 export const Button = styled.button`
   display: flex;
   align-items: center;
+  height: 100%;
   height: 70px;
   background: #e50914;
   color: white;
@@ -33,39 +38,31 @@ export const Button = styled.button`
   font-size: 26px;
   border: 0;
   cursor: pointer;
-
+  img {
+    margin-left: 10px;
+    filter: brightness(0) invert(1);
+    width: 24px;
+    @media (max-width: 1000px) {
+      width: 16px;
+    }
+  }
   &:hover {
     background: #f40612;
   }
-
   @media (max-width: 1000px) {
     height: 50px;
     font-size: 16px;
     margin-top: 20px;
     font-weight: bold;
   }
-  img {
-    margin-left: 10px;
-    filter: brightness(0) invert(1);
-    width: 24px;
-
-    @media (max-width: 1000px) {
-      width: 16px;
-    }
-  }
 `;
+
 export const Text = styled.p`
   font-size: 19.2px;
   color: white;
   text-align: center;
-
   @media (max-width: 600px) {
     font-size: 16px;
     line-height: 22px;
   }
-`;
-
-export const Break = styled.div`
-  flex-flex-basis: 100%;
-  height: 0;
 `;
