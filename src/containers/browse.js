e46a0e171348797d148a99008a212ce01f3fa727
nam <nam2711@hotmail.fr>
2020-11-19 15:06:12 +0100

    Player components starting jest test

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,15 +1,17 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { SelectProfileContainer } from './profile';
-import { FirebaseContext } from '../context/firebase';
-import { Card, Header, Loading } from '../components';
+import React, { useState, useEffect, useContext } from 'react';
+import Fuse from 'fuse.js';
+import { Card, Header, Loading, Player } from '../components';
 import * as ROUTES from '../constants/routes';
 import logo from '../logo.svg';
+import { FirebaseContext } from '../context/firebase';
+import { SelectProfileContainer } from './profile';
+import { FooterContainer } from './footer';
 
-export default function BrowseContainer({ slides }) {
-  const [category, setCategory] = useState('series');
-  const [searchTerm, setSearchTerm] = useState('');
+export function BrowseContainer({ slides }) {
+  const [category, setCategory] = useState('films');
   const [profile, setProfile] = useState({});
   const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
   const [slideRows, setSlideRows] = useState([]);
 
   const { firebase } = useContext(FirebaseContext);
@@ -25,49 +27,62 @@ export default function BrowseContainer({ slides }) {
     setSlideRows(slides[category]);
   }, [slides, category]);
 
+  useEffect(() => {
+    const fuse = new Fuse(slideRows, {
+      keys: ['data.description', 'data.title', 'data.genre']
+    });
+    const results = fuse.search(searchTerm).map(({ item }) => item);
+
+    if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+      setSlideRows(results);
+    } else {
+      setSlideRows(slides[category]);
+    }
+  }, [searchTerm]);
+
   return profile.displayName ? (
     <>
       {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+
       <Header src="joker1" dontShowOnSmallViewPort>
-        <Header background={false}>
-          <Header.Frame>
-            <Header.Group>
-              <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
-              <Header.TextLink
-                active={category == 'series' ? 'true' : 'false'}
-                onClick={() => setCategory('series')}
-              >
-                Series
-              </Header.TextLink>
-              <Header.TextLink
-                active={category == 'flims' ? 'true' : 'false'}
-                onClick={() => setCategory('films')}
-              >
-                Films
-              </Header.TextLink>
-            </Header.Group>
-            <Header.Group>
-              <Header.Search
-                searchTerm={searchTerm}
-                setSearchTerm={setSearchTerm}
-              />
-              <Header.Profile>
-                <Header.Picture src={user.photoURL} />
-                <Header.Dropdown>
-                  <Header.Group>
-                    <Header.Picture src={user.photoURL} />
-                    <Header.TextLink>{user.displayName}</Header.TextLink>
-                  </Header.Group>
-                  <Header.Group>
-                    <Header.TextLink onClick={() => firebase.auth().signOut}>
-                      Sign out
-                    </Header.TextLink>
-                  </Header.Group>
-                </Header.Dropdown>
-              </Header.Profile>
-            </Header.Group>
-          </Header.Frame>
-        </Header>
+        <Header.Frame>
+          <Header.Group>
+            <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+            <Header.TextLink
+              active={category === 'series' ? 'true' : 'false'}
+              onClick={() => setCategory('series')}
+            >
+              Series
+            </Header.TextLink>
+            <Header.TextLink
+              active={category === 'films' ? 'true' : 'false'}
+              onClick={() => setCategory('films')}
+            >
+              Films
+            </Header.TextLink>
+          </Header.Group>
+          <Header.Group>
+            <Header.Search
+              searchTerm={searchTerm}
+              setSearchTerm={setSearchTerm}
+            />
+            <Header.Profile>
+              <Header.Picture src={user.photoURL} />
+              <Header.Dropdown>
+                <Header.Group>
+                  <Header.Picture src={user.photoURL} />
+                  <Header.TextLink>{user.displayName}</Header.TextLink>
+                </Header.Group>
+                <Header.Group>
+                  <Header.TextLink onClick={() => firebase.auth().signOut()}>
+                    Sign out
+                  </Header.TextLink>
+                </Header.Group>
+              </Header.Dropdown>
+            </Header.Profile>
+          </Header.Group>
+        </Header.Frame>
+
         <Header.Feature>
           <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
           <Header.Text>
@@ -80,6 +95,7 @@ export default function BrowseContainer({ slides }) {
           <Header.PlayButton>Play</Header.PlayButton>
         </Header.Feature>
       </Header>
+
       <Card.Group>
         {slideRows.map((slideItem) => (
           <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
@@ -98,11 +114,15 @@ export default function BrowseContainer({ slides }) {
               ))}
             </Card.Entities>
             <Card.Feature category={category}>
-              <p>Hello</p>
+              <Player>
+                <Player.Button />
+                <Player.Video src="/videos/bunny.mp4" />
+              </Player>
             </Card.Feature>
           </Card>
         ))}
       </Card.Group>
+      <FooterContainer />
     </>
   ) : (
     <SelectProfileContainer user={user} setProfile={setProfile} />
